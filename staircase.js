dca32f01d1b775697ff2b9ed5b44176373d518fe
Syed Sameer <sameer.shah12570@gmail.com>
2020-09-22 06:48:43 +0500

    staircase done

--- staircase.js
+++ staircase.js
@@ -1,4 +1,13 @@
 function staircase(n) {
-  console.log(n);
+  let shape = "";
+  for (let i = n; i > 0; i--) {
+    let space = "";
+
+    for (let j = i; j > 1; j--) {
+      space += " ";
+    }
+    shape += "#";
+    return space + shape;
+  }
 }
 staircase(6);
